08eb76ef92cb029e1dd7f6f34050c6d78c6de343
Lucas Misael Barbosa Do Nascimento <lucas.nascimento@docente.pr.senac.br>
2026-09-03 22:26:08 -0300

    Aula 05

---
+++ aula_05/exemplo_while.php
@@ -0,0 +1,23 @@
+<?php
+
+// solicita ao usuario o numero inicial da contagem
+$inicio = (int) readline("Informe o número inicial: ");
+
+// solicita ao usuario o numero final da contagem
+$fim = (int) readline("Informe o número final: ");
+
+//Cria o contador começando pelo numero inicial 
+$contador = $inicio;
+
+//Enquanto o contador for menor ou igual ao numero final, o bloco será repetido 
+while($contador <= $fim ){
+
+    //Exibe o valor atual do contador 
+    echo "Contador: {$contador}" . PHP_EOL;
+
+    //contador ++;
+    $contador = $contador +1;
+}
+
+echo "Fim da contagem :) ". PHP_EOL;
+
